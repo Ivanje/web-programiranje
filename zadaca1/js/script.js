c28c6df28accd502257b4b8af2fe50ea59e14514
Ivanje <ivan_jelik_c@hotmail.com>
2016-10-25 00:00:14 +0200

    Add files via upload

---
+++ zadaca1/js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$("#square").hover(function(){
+		$(this).css("background-color","#1F7FC9");
+	}, function(){
+		$(this).css("background-color","#BF0000");
+	});
+});
